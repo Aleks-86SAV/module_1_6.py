aa22efaa1753b21d1d05f45477e799615e1672ae
Alexander Smirnov <sanyablask@gmail.com>
2025-01-19 16:17:36 +0300

    Add files via upload

---
+++ module_1_6..py
@@ -0,0 +1,17 @@
+#my_dict = {'Valera':1963,'Alex':1982,'Alexander':1986}
+#print(my_dict)
+#print(my_dict['Alex'])
+#my_dict['Gena'] = 1957
+#print(my_dict)
+#my_dict['Dmitry'] = 1995
+#print(my_dict)
+#my_dict['Zoya'] = 1962
+#print(my_dict)
+#del my_dict['Alex']
+#print(my_dict)
+my_set ={1,'Аpple',3.14, 1,'Bаnаnа','Apple'}
+1.print("Set:,my_set)
+2.my_set.add(42)
+my_set.add((5,6,7))
+3.my_set.discard('Banana')
+4.print("Modified set:",my_set)
